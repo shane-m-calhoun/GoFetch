063c20cdbb27899aef010dd4b0d169f291fdc7b6
shane-m-calhoun <shane.m.calhoun@gmail.com>
2018-04-13 20:03:28 -0600

    Added all puppy lessons
    Added bathing, nail triming, and brushing teeth. There are now 5 lessons for puppies.
    
    Moved "come here" to beginer.
    Beginner now has two lessons (Clicker training and come here). Now gonna finish up sit and then add lay down and stay.

--- GoFetch/app/src/main/java/edu/western/cs/gofetch/createLessons/Beginner.java
+++ GoFetch/app/src/main/java/edu/western/cs/gofetch/createLessons/Beginner.java
@@ -26,6 +26,20 @@ public class Beginner {
 
         list.add(clickerTraining);
 
+        Lesson comeHere = new Lesson();
+
+        comeHere.setTitle("\"Come Here\"");
+        comeHere.setDescription("25pts - Not only is it nice to have your dog come to you when you want some loving, it also can save your dog's life. " +
+                "For example, if your dog just ran out of the house and is headed straight towards the road, \"Come Here,\" could prevent your dog from getting hit by a car. \n \n" +
+                "With this lesson, it is best to start in your house, then your yard or a grass area, and progress to a busy area with lots of distractions. " +
+                "Remember, the clicker can be used to get their attention and it will really help with this lesson \n \n");
+        comeHere.setSteps("1) Put your puppy on a leash. The longer the leash, the better. A 10-foot to 20-f0ot leash is the best and this length will also help with off leash training. \n \n" +
+                "2) Hold on to the leash and say, \"Come here,\" while quickly moving backwards. \n \n" +
+                "3) Keep moving back until your puppy gets all the way to you. If your puppy is not responding, give a light tug on the leash while saying, \"Come here.\" \n \n" +
+                "4) Once your puppy catches up to you say, \"Yes! Good boy/girl!\" Then give your dog a treat. ");
+
+        list.add(comeHere);
+
         Lesson test = new Lesson();
 
         test.setTitle("Sit Training");
