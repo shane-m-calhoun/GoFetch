1f94a9e858728818caa81ff93ff979538f6381b5
shane-m-calhoun <shane.m.calhoun@gmail.com>
2018-04-14 23:18:11 -0600

    All lessons except for the advanced
    Made every lesson except for the advanced lessons.
    
    Advanced lessons already selected, just need to add the details and steps.

--- GoFetch/app/src/main/java/edu/western/cs/gofetch/createLessons/Advanced.java
+++ GoFetch/app/src/main/java/edu/western/cs/gofetch/createLessons/Advanced.java
@@ -12,13 +12,51 @@ public class Advanced {
     public static ArrayList<Lesson> advancedLessons(){
         ArrayList<Lesson> list = new ArrayList<>();
 
-        Lesson test = new Lesson();
 
-        test.setTitle("Off-Leash Training");
-        test.setDescription("This is just a test");
-        test.setSteps("1) test \n" +
-                "2) test");
-        list.add(test);
+        Lesson headDown = new Lesson();
+
+        headDown.setTitle("Rollover");
+        headDown.setDescription("This is just a headDown");
+        headDown.setSteps("1) headDown \n" +
+                "2) headDown");
+        list.add(headDown);
+
+
+
+        Lesson lookAtMe = new Lesson();
+
+        lookAtMe.setTitle("Rollover");
+        lookAtMe.setDescription("This is just a lookAtMe");
+        lookAtMe.setSteps("1) lookAtMe \n" +
+                "2) lookAtMe");
+        list.add(lookAtMe);
+
+
+        Lesson putAway = new Lesson();
+
+        putAway.setTitle("Rollover");
+        putAway.setDescription("This is just a putAway");
+        putAway.setSteps("1) putAway \n" +
+                "2) putAway");
+        list.add(putAway);
+        
+
+        Lesson heel = new Lesson();
+
+        heel.setTitle("Rollover");
+        heel.setDescription("This is just a heel");
+        heel.setSteps("1) heel \n" +
+                "2) heel");
+        list.add(heel);
+        
+        
+        Lesson offLeash = new Lesson();
+
+        offLeash.setTitle("Off-Leash Training");
+        offLeash.setDescription("This is just a offLeash");
+        offLeash.setSteps("1) offLeash \n" +
+                "2) offLeash");
+        list.add(offLeash);
 
         return list;
     }
